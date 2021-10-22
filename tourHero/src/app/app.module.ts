9398003bd95b1bb3f7d8e554f7d5c9bd6a2b6a23
elizaoliveira88 <eliza_oliveira88@outlook.com>
2021-10-22 08:46:11 -0300

    repos atomic

--- tourHero/src/app/app.module.ts
+++ tourHero/src/app/app.module.ts
@@ -5,6 +5,8 @@ import { HttpClientModule } from '@angular/common/http';
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { InMemoryDataService } from './services/in-memory-data.service';
 
+import { MatButtonModule } from '@angular/material/button';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './componets/heroes/heroes.component';
@@ -12,6 +14,14 @@ import { HeroDetailComponent } from './componets/hero-detail/hero-detail.compone
 import { MessagesComponent } from './componets/messages/messages.component';
 import { DashboardComponent } from './componets/dashboard/dashboard.component';
 import { HeroSearchComponent } from './componets/hero-search/hero-search.component';
+import { ButtonComponent } from './componets/atoms/button/button.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { H1Component } from './componets/atoms/h1/h1.component';
+import { SearchComponent } from './componets/atoms/search/search.component';
+import { H2Component } from './componets/atoms/h2/h2.component';
+import { LabelComponent } from './componets/atoms/label/label.component';
+import { InputComponent } from './componets/atoms/input/input.component';
+
 
 @NgModule({
   imports: [
@@ -21,7 +31,9 @@ import { HeroSearchComponent } from './componets/hero-search/hero-search.compone
     HttpClientModule,
     HttpClientInMemoryWebApiModule.forRoot(
       InMemoryDataService, { dataEncapsulation: false }
-    )
+    ),
+    BrowserAnimationsModule,
+    MatButtonModule,
   ],
   declarations: [
     AppComponent,
@@ -29,7 +41,14 @@ import { HeroSearchComponent } from './componets/hero-search/hero-search.compone
     HeroesComponent,
     HeroDetailComponent,
     MessagesComponent,
-    HeroSearchComponent
+    HeroSearchComponent,
+    ButtonComponent,
+    H1Component,
+    SearchComponent,
+    H2Component,
+    LabelComponent,
+    InputComponent,
+    
   ],
   bootstrap: [ AppComponent ]
 })
